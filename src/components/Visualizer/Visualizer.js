81aeeb4512f40945e332343eb05d758d7eb46033
richard-dinh <richarddinh@gmail.com>
2021-03-29 16:16:24 -0700

    set up array generation

--- src/components/Visualizer/Visualizer.js
+++ src/components/Visualizer/Visualizer.js
@@ -1,17 +1,35 @@
 import React, {useState, useEffect} from 'react'
-
+import "./Visualizer.css";
 const Visualizer = () => {
 
     const [array, setArray] = useState([])
 
 
     useEffect( () => {
-        console.log('Populate array with random numbers')
+        //TODO: allow for adjusing of array length
+        let arr = []
+        // populate array with random numbers between 10 - 1000
+        // allow for duplicate numbers as sorting algorithms should be able to handle duplicate numbers
+        for(let i = 0; i < 50; i++){
+            arr.push(generateRandomNumber(10, 100))
+        }
+        setArray(arr)
     }, [])
+
+    const generateRandomNumber = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1)) + min
+    }
+
+
     return(
-        <p>
-            Array goes here
-        </p>
+        <div className = 'array_container'>
+            {array.map((value, index) => {
+                return(
+                    <div key = {index} className = 'array_column' style = {{height: `${value}px`}}>
+                    </div>
+                )
+            })}
+        </div>
     )
 }
 
