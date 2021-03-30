4dbfc6c1d2941cc7175a059dc02ada92e6661e73
richard-dinh <richarddinh@gmail.com>
2021-03-29 17:40:54 -0700

    added options section

--- src/components/Visualizer/Visualizer.js
+++ src/components/Visualizer/Visualizer.js
@@ -10,8 +10,8 @@ const Visualizer = () => {
         let arr = []
         // populate array with random numbers between 10 - 1000
         // allow for duplicate numbers as sorting algorithms should be able to handle duplicate numbers
-        for(let i = 0; i < 50; i++){
-            arr.push(generateRandomNumber(10, 100))
+        for(let i = 0; i < 150; i++){
+            arr.push(generateRandomNumber(10, 200))
         }
         setArray(arr)
     }, [])
@@ -20,15 +20,43 @@ const Visualizer = () => {
         return Math.floor(Math.random() * (max - min + 1)) + min
     }
 
-
+    const generateNewArray = () => {
+        let arr = []
+        for (let i = 0; i < 150; i++) {
+            arr.push(generateRandomNumber(10, 200));
+        }
+        setArray(arr);
+    }
     return(
-        <div className = 'array_container'>
-            {array.map((value, index) => {
-                return(
-                    <div key = {index} className = 'array_column' style = {{height: `${value}px`}}>
-                    </div>
-                )
-            })}
+        <div className = 'container'>
+            <ul className = 'options'>
+                <h3>Sorting Algorithms</h3>
+                <li>
+                    <button>Bubble Sort</button>
+                </li>
+                <li>
+                    <button>Heap Sort</button>
+                </li>
+                <li>
+                    <button>Quick Sort</button>
+                </li>
+                <hr class="divider"></hr>
+                <li>
+                    <button onClick = {generateNewArray}>Generate New Array</button>
+                </li>
+                <hr class="divider"></hr>
+                <li>
+                    <button>Sort</button>
+                </li>
+            </ul>
+            <div className = 'array_container'>
+                {array.map((value, index) => {
+                    return(
+                        <div key = {index} className = 'array_column' style = {{height: `${value}px`}}>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
